1882d9712beac38b26a070d61b0adbe74d651263
Lyu-ku-tan <studentluk@mail.ru>
2020-08-19 10:24:25 +0300

    final v1

--- src/main/java/com/example/weather/Application.java
+++ src/main/java/com/example/weather/Application.java
@@ -1,14 +1,17 @@
 package com.example.weather;
 
 
+import com.example.weather.clients.CountryInfoServiceClient;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext context = SpringApplication.run(JaxbConfig.class, args);
+        CountryInfoServiceClient countryInfoServiceClient = context.getBean(CountryInfoServiceClient.class);
+        System.out.println(countryInfoServiceClient.getCountryNameByISOCode("AD").getCountryNameResult());
+
     }
 
 }
