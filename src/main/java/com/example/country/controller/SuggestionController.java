213c1be906ef3a13f1479baa186ac7e3a2ba7e3d
Lyu-ku-tan <studentluk@mail.ru>
2020-09-07 10:22:10 +0300

    fix vue.js file optimize import

--- src/main/java/com/example/country/controller/SuggestionController.java
+++ src/main/java/com/example/country/controller/SuggestionController.java
@@ -6,22 +6,19 @@ import com.example.country.services.searchPerson.SearchFioService;
 import com.example.country.services.searchPerson.SearchIdentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("suggestion")
 public class SuggestionController {
 
     @Autowired
     private CountryInfoServiceClient countryInfoServiceClient;
 
-    @RequestMapping(value = "/suggestion", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    public String suggestions(@RequestParam("searchstr") String searchstr) {
+    @PostMapping
+    public String suggestions(@RequestBody String searchstr) {
         Person person = searchstr.matches("[-+]?\\d+")  ? new SearchIdentService().search(searchstr)  : new SearchFioService().search(searchstr);
         return countryInfoServiceClient.getCountryNameByISOCode(person.getCountry()).getCountryNameResult();
     }
